7d6d66f1bfaee710519464847dbb0db3af45328f
KhanhNgo <62138525+khanh7516@users.noreply.github.com>
2025-04-28 20:03:12 +0700

    fix format

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,24 +3,18 @@ import { CreateProductDto } from './dtos/create-product.dto';
 
 @Controller('products')
 export class ProductController {
-    @Get()
-    listProducts(){
-        return "list of product";
-    }
+  @Get()
+  listProducts() {
+    return 'list of product';
+  }
 
-    @Get("/:id")
-    getProductById(@Param('id') id: string) {
-        return "product";
-    }
+  @Get('/:id')
+  getProductById(@Param('id') id: string) {
+    return 'product';
+  }
 
-    @Post()
-    createProduct(@Body() body: CreateProductDto) {
-        return "product created";
-    }
-
-
-
-
-
+  @Post()
+  createProduct(@Body() body: CreateProductDto) {
+    return 'product created';
+  }
 }
-
